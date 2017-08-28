d5646b42c821050286a7ae7c611ccdda8da0fdb5
KilluaChen <larry_e@163.com>
2017-08-28 14:14:48 +0800

    first commit

---
+++ icc/autoload.js
@@ -0,0 +1,51 @@
+/**
+ * Created by killua on 17/8/28.
+ */
+var prev = '';
+var cs = 0;
+dl();
+function dl() {
+    var is_return = false;
+    var len = $('.x-list-plain').length - 1;
+    var $children = $($('.x-list-plain')[len]).children();
+    $children.each(function (key, val) {
+        if (key == 0) {
+            var cu_val = $(val).html();
+            if (cu_val == prev) {
+                is_return = true;
+                return false
+            }
+            prev = cu_val
+        }
+        if (!$(val).is('.x-boundlist-selected')) {
+            $(val).click()
+        }
+    });
+    if (is_return) {
+        return false
+    }
+    var len = $('.x-tbar-page-next').length - 1;
+    if (len <= 3) {
+        return false
+    }
+    $('.x-tbar-page-next')[len].click();
+    cs = new Date().getTime() / 1000;
+    check()
+}
+function check(len) {
+    var cu = new Date().getTime() / 1000;
+    if (cu > cs + 10) {
+        console.log('超过10秒');
+        return false
+    }
+    var len = $('.x-list-plain').length - 1;
+    var $c = $($('.x-list-plain')[len]).children();
+    setTimeout(function () {
+        if ($c[0].innerHTML == prev) {
+            check()
+        } else {
+            console.log('new:' + $c[0].innerHTML);
+            dl()
+        }
+    }, 500)
+}
